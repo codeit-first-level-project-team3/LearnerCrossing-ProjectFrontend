f44c57596ebceee99726654d26984f2631f85eef
DESKTOP-EAN86Q6\vb120 <vb1201@naver.com>
2025-08-26 19:32:05 +0900

    Home 페이지 리셋

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,15 +7,6 @@ function Home() {
   return (
     <>
       <h1>Hello World!</h1>
-      <Label labelId="id" labelName="스터디이름" />
-      <Toast text="비밀번호가 일치하지 않습니다. 다시 입력해주세요." type="warning"/>
-      <Title
-        nickName="이유디"
-        title="개발 공장"
-        isColorBlack={false}
-        highlightColor="yellow"
-      />
-      <Sticker color="green" num={5}/>
     </>
   );
 }
