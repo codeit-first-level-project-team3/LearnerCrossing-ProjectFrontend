9c938ff83970adc8c722b6c30db7f60b99fd8eb5
sungminiioo <sungminiioo426@gmail.com>
2025-08-26 16:15:12 +0900

    GNB에 필요한 컴포넌트들 전부 추가 및 반응형으로 제작

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import './global.css'
 import router from "./router";
 import { RouterProvider } from "react-router-dom";
+import GNB from "./components/organisms/GNB/GNB";
 
 function App() {
   return (
-    <RouterProvider router={router}/> 
+    <div>
+    <RouterProvider router={router}/>
+    {/* gnb 스터디 만들기 버튼이 없는 버전 test */}
+    <GNB />
+    {/* gnb 스터디 만들기 버튼이 있는 버전 test */}
+    <GNB showCreateStudy={true} />
+    </div> 
   );
 }
 
