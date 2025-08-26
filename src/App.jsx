25eb3177d5921b5a024d55fb5a5dc3aabccd8fbe
sungminiioo <sungminiioo426@gmail.com>
2025-08-26 16:31:23 +0900

    molecules/HabitButton 컴포넌트 생성(반응형까지)

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './global.css'
 import router from "./router";
 import { RouterProvider } from "react-router-dom";
 import GNB from "./components/organisms/GNB/GNB";
+import HabitButton from './components/molecules/HabitButton/HabitButton';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
     <RouterProvider router={router}/>
     {/* gnb 스터디 만들기 버튼이 없는 버전 test */}
     <GNB />
+    <HabitButton />
     {/* gnb 스터디 만들기 버튼이 있는 버전 test */}
     <GNB showCreateStudy={true} />
     </div> 
