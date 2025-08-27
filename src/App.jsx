972343c8f893d75333975da804d6a199c93d3e18
sungminiioo <sungminiioo426@gmail.com>
2025-08-27 09:52:05 +0900

    어제 css 파일 root 적용

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,11 @@ import HabitButton from './components/molecules/HabitButton/HabitButton';
 import MakeButton from './components/molecules/MakeButton/MakeButton';
 import Search from './components/molecules/Search/Search';
 import Sort from './components/molecules/Sort/Sort';
-import Input from './components/atoms/Input/Input';
+import INput from './components/atoms/Input/Input'
 
 function App() {
   return (
-    <div>
     <RouterProvider router={router} />
-    {/* 일반 텍스트 인풋 */}
-      <Input type="text" placeholder="아이디를 입력하세요" />
-
-      {/* 비밀번호 인풋 (눈 아이콘 없음) */}
-      <Input type="password" placeholder="비밀번호를 입력하세요" />
-
-      {/* 비밀번호 인풋 (눈 아이콘 토글 표시) */}
-      <Input type="password" placeholder="비밀번호를 입력하세요" showToggle />
-    </div>
   );
 }
 
