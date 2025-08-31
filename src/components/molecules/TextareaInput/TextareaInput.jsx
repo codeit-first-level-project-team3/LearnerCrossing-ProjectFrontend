9c54d4e919326d3c3cb35935725547a18095f099
sungminiioo <sungminiioo426@gmail.com>
2025-08-31 13:23:05 +0900

    LabeledTextareaInput 생성

--- src/components/molecules/TextareaInput/TextareaInput.jsx
+++
@@ -1,5 +0,0 @@
-import styles from "./TextareaInput.module.css";
-
-export default function TextareaInput({ placeholder, ...props }) {
-  return <textarea placeholder={placeholder} className={styles.textarea} {...props} />;
-}
