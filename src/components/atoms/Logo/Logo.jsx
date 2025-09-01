66228eb854ce8880a3f4c10840c34911d03f1932
sungminiioo <sungminiioo426@gmail.com>
2025-09-01 16:29:09 +0900

    GNB 로고 클릭 시 홈페이지로 이동하는 링크 추가

--- src/components/atoms/Logo/Logo.jsx
+++ src/components/atoms/Logo/Logo.jsx
@@ -1,11 +1,15 @@
+import { Link } from "react-router-dom";
 import styles from "./Logo.module.css";       
 import logo from "../../../assets/Logo.svg";  
+
 export default function Logo() {
   return (
-    <img
-      src={logo}            
-      alt="로고"
-      className={styles.logo} 
-    />
+    <Link to="/">
+      <img
+        src={logo}            
+        alt="로고"
+        className={styles.logo} 
+      />
+    </Link>
   );
 }
