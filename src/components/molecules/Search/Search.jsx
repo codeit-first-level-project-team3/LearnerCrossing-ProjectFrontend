dfd308adb0f8904896ca94053b8b48bccf463ada
sungminiioo <sungminiioo426@gmail.com>
2025-09-01 18:00:24 +0900

    Search에 onChange 추가

--- src/components/molecules/Search/Search.jsx
+++ src/components/molecules/Search/Search.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import styles from "./Search.module.css";
 import searchIcon from "../../../assets/search.svg";
 
-export default function Search({ placeholder = "검색" }) {
+export default function Search({ placeholder = "검색", onChange }) {
   return (
     <div className={styles.searchBox}>
       <img src={searchIcon} alt="검색 아이콘" className={styles.searchIcon} />
-      <input type="text" placeholder={placeholder} />
+      <input type="text" placeholder={placeholder} onChange={onChange} />
     </div>
   );
 }
