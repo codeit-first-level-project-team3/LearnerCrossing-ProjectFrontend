0ce9f72639a98890e172c953ca6097870348f4d3
sungminiioo <sungminiioo426@gmail.com>
2025-09-01 13:03:44 +0900

    GNB 스터디 만들기 버튼 studyCreate 바로 가기 추가

--- src/components/molecules/StudyCreateButton/StudyCreateButton.jsx
+++ src/components/molecules/StudyCreateButton/StudyCreateButton.jsx
@@ -1,10 +1,21 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import Button from "../../atoms/Button/Button"; 
 import styles from "./StudyCreateButton.module.css"; 
 
 export default function StudyCreateButton({ text = "스터디 만들기", ...props }) {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/studyCreate"); 
+  };
+
   return (
-    <Button className={styles.studyCreateBtn} {...props}>
+    <Button
+      className={styles.studyCreateBtn}
+      onClick={handleClick}
+      {...props}
+    >
       {text}
     </Button>
   );
