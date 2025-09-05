3c4a82b93a2ccc3b401060de558fa6111b52eea0
apple <apple@apples-MacBook-Air.local>
2025-09-05 18:00:39 +0900

    feat: emojis get 구현

--- src/api/studyAPI.js
+++ src/api/studyAPI.js
@@ -29,7 +29,7 @@ export async function getStudy(id) {
       // 상태 코드 확인 (200~299 범위만 성공)
       if (res.status < 200 || res.status >= 300) {
         throw new Error(
-          `습관들을 불러오는데 실패했습니다. (status: ${res.status})`
+          `스터디를 불러오는데 실패했습니다. (status: ${res.status})`
         );
       }
       return res.data;
--- src/pages/StudyDetail/StudyDetail.jsx
+++ src/pages/StudyDetail/StudyDetail.jsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-import { useAutoAsync } from "../../hooks/useAsync.js";
+import { useAutoAsync, useActionAsync } from "../../hooks/useAsync.js";
 import { deleteStudy, getStudy } from "../../api/studyAPI.js";
 import { getHabitList } from "../../api/habitAPI.js";
+import { addStudyEmoji, getStudyEmojis } from "../../api/emojiAPI.js";
 
 import Toast from "../../components/atoms/Toast.jsx";
 import CheerTagGroup from "../../components/molecules/CheerTagGroup/CheerTagGroup.jsx";
@@ -23,10 +24,16 @@ function StudyDetail() {
   const [warning, setWarning] = useState(false); // 경고창
   const navigate = useNavigate(); // 페이지 이동
   const [habits, setHabits] = useState([]); // habits 상태
+  const [emojis, setEmojis] = useState([]);
 
-  const [isHabitsLoading, habitsLoadingError, getHabitsAsync] = useAutoAsync(getHabitList); // 습관 가져오기 로딩,에러처리
+  const [isHabitsLoading, habitsLoadingError, getHabitsAsync] =
+    useAutoAsync(getHabitList); // 습관 가져오기 로딩,에러처리
+  const [isEmojisLoading, EmojisLoadingError, getEmojisAsync] =
+    useAutoAsync(getStudyEmojis); // 이모지 가져오기 로딩, 에러처리
+  const [addEmojiLoading, addEmojiLodingError, addEmojisAsync] =
+    useActionAsync(addStudyEmoji); // 이모지 추가 로딩, 에러처리
 
-  const studyId = 1; // 임시 스터디 아이디
+  const studyId = 3; // 임시 스터디 아이디
   const pwd = "1234"; // 임시 비밀번호
 
   // 유니코드 -> 이모지
@@ -35,23 +42,33 @@ function StudyDetail() {
       ...unified.split("-").map((u) => parseInt(u, 16))
     );
   };
-  // 임시 이모지 상태
-  const [emojis, setEmojis] = useState({
-    1: { emoji: unifiedToEmoji("1f47d"), count: 10 },
-    // 2: { emoji: "👽", count: 78 },
-  });
+
+  const handleEmojisLoad = async () => {
+    try {
+      const result = await getEmojisAsync(studyId);
+      setEmojis(result);
+    } catch (error) {
+      console.error("습관 불러오기 실패:", error.message);
+    }
+  };
 
   // 이모지 카운트 증가 함수
-  function increaseCnt(id) {
-    setEmojis((prev) => ({
-      ...prev,
-      [id]: {
-        ...prev[id],
-        count: prev[id].count + 1,
-      },
-    }));
+  async function increaseCnt(id) {
+    // setEmojis((prev) => ({
+    //   ...prev,
+    //   [id]: {
+    //     ...prev[id],
+    //     count: prev[id].count + 1,
+    //   },
+    // }));
     // console.log(chosenEmoji) // 이모지 픽커 이모지 확인
-    // console.log("id:", id); // 현재 클릭 아이디 확인용
+    console.log("id:", id); // 현재 클릭 아이디 확인용
+     try {
+      const result = await addEmojisAsync(studyId, id);
+      setEmojis(result);
+    } catch (error) {
+      console.error("습관 불러오기 실패:", error.message);
+    }
   }
   // 이모지 추가 함수
   function addEmoji(newEmoji) {
@@ -167,10 +184,10 @@ function StudyDetail() {
     }
   };
 
-  // // 스터디 데이터 값 확인용도
-  // useEffect(() => {
-  //   console.log("studyData가 업데이트됨:", studyData);
-  // }, [studyData]);
+  // 로드 된 데이터 값 확인용도
+  useEffect(() => {
+    console.log("studyData가 업데이트됨:", emojis.items?.length);
+  }, [emojis]);
 
   // 스터디(id:3)의 habits 가져오기
   const handleHabitsLoad = async () => {
@@ -186,6 +203,7 @@ function StudyDetail() {
     // 데이터 불러오기
     handleHabitsLoad();
     handleStudyLoad();
+    handleEmojisLoad();
   }, []);
 
   return (
@@ -214,7 +232,7 @@ function StudyDetail() {
         <StudyMain>
           <div className={styles.utilityBar}>
             <div className={styles.emojiBox}>
-              <CheerTagGroup emojis={emojis} onClick={increaseCnt} />
+              <CheerTagGroup emojis={emojis} onClick={increaseCnt} isLoading={isEmojisLoading}/>
               <EmojiPickerButton setChosenEmoji={setChosenEmoji} />
             </div>
             <div className={styles.quickLinks}>
