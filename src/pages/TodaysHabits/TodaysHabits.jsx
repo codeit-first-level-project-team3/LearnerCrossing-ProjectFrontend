d7835d229843d3bb1749ceb7ac8690dd55b800ed
DESKTOP-EAN86Q6\vb120 <vb1201@naver.com>
2025-09-10 17:50:41 +0900

    습관 allClear 이스터에그

--- src/pages/TodaysHabits/TodaysHabits.jsx
+++ src/pages/TodaysHabits/TodaysHabits.jsx
@@ -25,8 +25,9 @@ function convertToArray(weeklyClear) {
 }
 
 function HabitList({ today, habits, handleToggle, setIsModalOpen }) {
+  const isAllClear = habits.every(habit=>convertToArray(habit.weeklyClear)[today]);
   return (
-    <div className={styles.box}>
+    <div className={`${styles.box} ${isAllClear && styles.allClear}`}>
       <div className={styles.routineWrapper}>
         <div className={styles.titleDiv}>
           <p className={styles.title}>오늘의 습관</p>
