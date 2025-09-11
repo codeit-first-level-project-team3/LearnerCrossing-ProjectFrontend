623bd5915d59e3e4d9005ed71ba2a96fa5d26c00
sungminiioo <sungminiioo426@gmail.com>
2025-09-11 16:54:01 +0900

    공통 백그라운드 처리

---
+++ src/constants/backgrounds.js
@@ -0,0 +1,19 @@
+import bg1 from "../assets/backgrounds/bg1.svg";
+import bg2 from "../assets/backgrounds/bg2.svg";
+import bg3 from "../assets/backgrounds/bg3.svg";
+import bg4 from "../assets/backgrounds/bg4.svg";
+
+// DB에는 key만 저장, 실제 값은 Map으로 변환
+export const IMAGE_BACKGROUNDS = {
+  BG1: bg1,
+  BG2: bg2,
+  BG3: bg3,
+  BG4: bg4,
+};
+
+export const COLOR_BACKGROUNDS = {
+  GREEN: "#E1EDDE",
+  YELLOW: "#FFF1CC",
+  PINK: "#FDE0E9",
+  BLUE: "#E0F1F5",
+};
