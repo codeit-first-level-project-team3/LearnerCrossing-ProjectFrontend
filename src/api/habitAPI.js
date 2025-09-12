d240d3d728549ccad1669af02bffae6c3de77140
DESKTOP-EAN86Q6\vb120 <vb1201@naver.com>
2025-09-12 13:22:24 +0900

    콘솔 메세지 지우기

--- src/api/habitAPI.js
+++ src/api/habitAPI.js
@@ -16,52 +16,6 @@ export async function getHabitList(studyId){
     return result;
 }
 
-/* 비밀번호 방식 */
-
-// // POST
-// export async function createHabit(studyId, data){
-//     const result = api.post(`/studies/${studyId}/habits`, data)
-//         .then(res => {
-//             if (res.status < 200 || res.status >= 300) {
-//                 throw new Error(`습관을 추가하는 데 실패했습니다. (status: ${res.status})`);
-//             }
-//             return res.data;
-//         })
-//         .catch(error => {
-//             console.error(error);
-//         });
-//     return result;
-// }
-// // PATCH
-// export async function updateHabit(studyId, habitId, data){
-//     const result = api.patch(`/studies/${studyId}/habits/${habitId}`, data)
-//         .then(res => {
-//             if (res.status < 200 || res.status >= 300) {
-//                 throw new Error(`습관을 수정하는 데 실패했습니다. (status: ${res.status})`);
-//             }
-//             return res.data;
-//         })
-//         .catch(error => {
-//             console.error(error);
-//         });
-//     return result;
-// }
-// // DELETE
-// export async function deleteHabit(studyId, habitId, pw){
-//     const config = {data: { password: pw} };
-//     const result = api.delete(`/studies/${studyId}/habits/${habitId}`, config) 
-//         .then(res => {
-//             if (res.status < 200 || res.status >= 300) {
-//                 throw new Error(`습관을 삭제하는 데 실패했습니다. (status: ${res.status})`);
-//             }
-//             return res.data;
-//         })
-//         .catch(error => {
-//             console.error(error);
-//         });
-//     return result;
-// }
-
 /* 토큰 방식 */
 // POST
 export async function createHabit(studyId, data, token){
--- src/pages/TodaysHabits/TodaysHabits.jsx
+++ src/pages/TodaysHabits/TodaysHabits.jsx
@@ -68,40 +68,12 @@ function TodaysHabits() {
     { to: "../focus", name: "오늘의 집중" },
     { to: "../", name: "홈" },
   ];
-
-  /* 
-    <크리티컬 에러>
-    일주일 넘어가기 전에 습관 페이지에 접속한 후에,
-    다음 주가 되어서 토글을 건드리면 분명 에러가 생긴다.
-    */
-  // const WeeklyReload = (habits) => {
-  //     if(!habits){return []}
-  //     const monday = new Date().getDate() - today;
-  //     const lastSaveDate = Math.min(...habits.map(e=>new Date(e.updatedAt)));//가장 오래된 수정 날짜 확인
-  //     if(lastSaveDate < monday){ //이번 주 월요일보다 이전 날짜면 모든 습관 달성도를 리셋.
-  //         habits.forEach(habit=>{
-  //             const body = {
-  //                 password: "1234",
-  //                 name: habit.name,
-  //                 weeklyClear: '0|0|0|0|0|0|0'
-  //             }
-  //             updateHabit(studyId, habitId, body);
-  //             habit.weeklyClear = '0|0|0|0|0|0|0';
-  //         });
-  //     }
-  //     return habits;
-  // }
-
+  
   //습관 컬럼을 받아온다.
   //일주일 치 달성 여부가 보인다.
   const handleHabitsLoad = async () => {
-    try {
-      const _habits = await getHabitList(studyId);
-      // const _habits = WeeklyReload(result); // 매주 리셋 함수
-      setHabits(_habits || []);
-    } catch (error) {
-      console.error("습관 불러오기 실패:", error.message);
-    }
+    const _habits = await getHabitList(studyId);
+    setHabits(_habits || []);
   };
 
   const handleToggle = (habitId, isClear) => {
@@ -110,17 +82,6 @@ function TodaysHabits() {
     const newWeeklyClear = convertToArray(habit.weeklyClear);
     newWeeklyClear[today] = isClear ? 0 : 1;
     habit.weeklyClear = convertToString(newWeeklyClear);
-
-    //console.log("토글 비밀번호: " + password);
-
-    /* 비밀번호 방식 */
-    // const body = {
-    //     name: habit.name,
-    //     weeklyClear: habit.weeklyClear,
-    //     password: password
-    // }
-
-    // updateHabit(studyId, habitId, body);
 
     /* 토큰 방식 */
     const body = {
